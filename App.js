56ef82742881f71953c7d78aba8423cc3ad072f6
tehniatfatima920-cell <tehniatfatima920@gmail.com>
2025-12-12 18:51:48 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import './App.css';
+//import NavbarCake from "./components/NavbarCake";
+import CakeWebsite from "./components/CakeWebsite";
+//import ProductList from "./components/ProductList";
+
+function App() {
+  return (
+    <div>
+     
+      <CakeWebsite />
+    
+    </div>
+  );
+}
+
+export default App;
+
+
