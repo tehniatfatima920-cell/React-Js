2d48894958613a27bf3c95a9132e7f76d826caf8
tehniatfatima920-cell <tehniatfatima920@gmail.com>
2025-12-12 19:05:59 +0500

    Add files via upload

---
+++ ProductList.js
@@ -0,0 +1,26 @@
+export const products = [
+  {
+    id: 1,
+    name: "Chocolate Fudge Cake",
+    price: 1800,
+    image: "https://images.unsplash.com/photo-1541976076758-25b32d6a2221"
+  },
+  {
+    id: 2,
+    name: "Red Velvet Cake",
+    price: 2200,
+    image: "https://images.unsplash.com/photo-1605806616949-1c9e9c8c04b4"
+  },
+  {
+    id: 3,
+    name: "Vanilla Celebration Cake",
+    price: 1500,
+    image: "https://images.unsplash.com/photo-1588196749597-9ff075ee6b5b"
+  },
+  {
+    id: 4,
+    name: "Black Forest Cake",
+    price: 1700,
+    image: "https://images.unsplash.com/photo-1549572189-0ba966affc6e"
+  }
+];
